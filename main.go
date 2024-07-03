2b3873817d7c57db35cf2bb62ead8254b950081c
gop <gameofpointers@proton.me>
2024-07-02 21:07:42 -0500

    Create a simulation engine and being able to mine a block

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
+import (
+	"github.com/shreekarashastry/blockchain/simulation"
+)
+
 const (
 	numHonestMiners uint64 = 20
 	numAdversary    uint64 = 5
 )
 
 func main() {
-	blockchain := NewBlockchain()
-	simulation := NewSimulation(blockchain, numHonestMiners, numAdversary)
+	blockchain := simulation.NewBlockchain()
+	simulation := simulation.NewSimulation(blockchain, numHonestMiners, numAdversary)
 	simulation.Start()
 }
